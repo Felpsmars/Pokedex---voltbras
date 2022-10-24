af60aa2ebcff17bcf1c5c4f55e51f848d0a1802b
Felipe Martins <mfelipeluiz99@gmail.com>
2022-10-23 23:10:47 -0300

    feat: adding assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,13 @@
+import config from './config.png';
+import trophy from './trophy.png';
+import info from './info.png';
+import menu from './menu.png';
+import voltbraslogo from './voltbraslogo.png';
+import misc from './misc.png';
+
+export { config };
+export { trophy };
+export { info };
+export { menu };
+export { voltbraslogo };
+export { misc };
